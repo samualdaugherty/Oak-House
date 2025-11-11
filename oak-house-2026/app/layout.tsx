9088fa9a6b5e0862243e62ccfee8ccd5ca4d9955
Sam Daugherty <samual.daugherty@gmail.com>
2025-11-11 11:43:16 -0600

    Adding Vercel analytics

--- oak-house-2026/app/layout.tsx
+++ oak-house-2026/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const montserrat = Montserrat({
@@ -61,6 +62,7 @@ export default function RootLayout({
           Skip to content
         </a>
         {children}
+        <Analytics />
       </body>
     </html>
   );
